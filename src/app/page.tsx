4e80c98d4aa9e0a8a70ac78c0e97a556923d24fb
shimeeuisuk <euisuk95@gmail.com>
2025-02-23 22:51:56 +0900

    feat: 메인페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import HomeWithObserver from "../components/HomeWithObserver";
+"use client";
 
-export default function Home() {
-  return <HomeWithObserver />;
-}
+import { ItemCategorySection } from "@/components/common/item-category-section";
+import { MainDisplaySection } from "@/components/main/display-section";
+import { SearchSection } from "@/components/common/search-section";
+import { Gap } from "@/components/common/gap/gap.component";
+import { ProductsSection } from "@/components/common/products-section/products-section.component";
+import { LogoSection } from "@/components/main/logo-section";
+import CategoryProvider from "@/context/category-context";
+
+const Home = () => {
+  return (
+    <div>
+      <MainDisplaySection />
+      <Gap flex={"column"} height={100} />
+      <LogoSection />
+      <Gap flex={"column"} height={100} />
+      <CategoryProvider>
+        <ItemCategorySection />
+        <Gap flex={"column"} height={50} />
+        <SearchSection />
+        <Gap flex={"column"} height={100} />
+        <ProductsSection />
+      </CategoryProvider>
+    </div>
+  );
+};
+
+export default Home;
